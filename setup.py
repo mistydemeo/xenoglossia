5ae00e167a89c120c6c2ab1418581949543fb722
Misty De Meo <mistydemeo@gmail.com>
2015-09-08 18:36:27 -0700

    Add description, author, license

--- setup.py
+++ setup.py
@@ -2,6 +2,10 @@ from setuptools import setup
 
 setup(
     name="xenoglossia",
+    description=" Robust(?) string manipulation language",
+    author="Misty De Meo",
+    author_email="mistydemeo@gmail.com",
+    license="kindest",
     version="0.1",
     packages=["xenoglossia"],
     entry_points={"console_scripts": ["xg = xenoglossia.main:main"]},
