8c61b6062f080a9a7c5f98d5fb92e64c512ea02f
Misty De Meo <mistydemeo@gmail.com>
2015-09-09 21:17:58 -0700

    0.2

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     author="Misty De Meo",
     author_email="mistydemeo@gmail.com",
     license="kindest",
-    version="0.1",
+    version="0.2",
     packages=["xenoglossia"],
     entry_points={"console_scripts": ["xg = xenoglossia.main:main"]},
     install_requires="pyparsing>=2,<3",
